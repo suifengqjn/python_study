aac7ba503f77966f3259a498130d129c9d6738cd
suifengqjn <qianjianeng@easestar.cn>
2019-10-19 14:10:38 +0800

    update

---
+++ 1基础语法/m.py
@@ -0,0 +1,12 @@
+print("中国，china")
+
+
+#格式化输出
+print ("我叫 %s 今年 %d 岁!" %('小明', 10))
+
+
+
+
+#type 查看变量类型
+name = "xiaoming"
+print(type(name))
---
+++ 8函数/func_doc.py
@@ -0,0 +1,15 @@
+
+# 文档注释
+
+def printLine(char, times):
+    """打印多行分隔符
+    :param char: 分隔符
+    :param times: 次数
+    """
+    c = 0
+    while c < times:
+        print(char, times)
+
+
+
+printLine("-", 5)
